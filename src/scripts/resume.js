6435652a35f03b027e8c5d1d560e850772fd8e68
Benjamin Vinnick <bvinnick@student.ubc.ca>
2025-05-03 11:53:12 -0700

    working experience

---
+++ src/scripts/resume.js
@@ -0,0 +1,131 @@
+export const resumeObj = {
+  "name": "Ben Vinnick",
+  "location": "Vancouver, BC",
+  "phone": "(604)-506-6024",
+  "email": "benvinnick@gmail.com",
+  "links": {
+    "portfolio": "https://bonvee-99.github.io/portfolio/",
+    "github": "https://github.com/bonvee-99",
+    "linkedin": "https://www.linkedin.com/in/ben-vinnick-452312166/"
+  },
+  "workExperience": [
+    {
+      "company": "TAIT",
+      "location": "Montreal, Quebec",
+      "title": "Software Developer Intern",
+      "startDate": "May 2024",
+      "endDate": "Aug 2024",
+      "highlights": [
+        "Collaborated on Feltboard, a Digital CMS for the Experience Design industry used by Google",
+        "Optimized storage efficiency by implementing shared media buckets, reducing redundant uploads by up to 5X",
+        "Enhanced user workflow by creating a “save as” feature, allowing users to easily replicate and modify data structures",
+        "Streamlined asset management by implementing media replacement, facilitating seamless updates without disrupting ongoing processes"
+      ]
+    },
+    {
+      "company": "TAIT",
+      "location": "Montreal, Quebec",
+      "title": "Software Developer Intern",
+      "startDate": "July 2022",
+      "endDate": "Aug 2023",
+      "highlights": [
+        "Developed a frontend dashboard using Vue to allow users to monitor key metrics within their connected devices",
+        "Facilitated dynamic schema relationships within the backend, empowering users to effortlessly create and manage intricate content structures by referencing one schema from another",
+        "Implemented queries and mutations for the GraphQL API to support a new device enrollment system",
+        "Designed a YAML parser to validate user input and provide guidance for precise data mapping",
+        "Developed a validation tool with TypeScript to maintain backward compatibility of payloads sent to 3rd-party devices",
+        "Implemented unit tests for GraphQL queries and mutations using Mocha, Chai, and Sinon",
+        "Deployed an image resize proxy using Docker, Nginx, and GKE to reduce service strain and optimize page load times",
+        "Developed payload analytics feature with GCP Cloud Functions and Pub/Sub to track payload latency across services",
+        "Deployed analytics feature with Terraform and connected it to Datadog for streamlined monitoring and analysis"
+      ]
+    },
+    {
+      "company": "University of British Columbia",
+      "location": "Vancouver, BC",
+      "title": "Teaching Assistant",
+      "startDate": "Sept 2021",
+      "endDate": "Dec 2022",
+      "highlights": [
+        "Taught fundamental programming skills and concepts such as recursion, graph traversal, testing, and debugging",
+        "Received a 100% favourable rating from > 70 students",
+        "Led weekly labs and office hours for over 1200 students over three academic semesters"
+      ]
+    }
+  ],
+  "projects": [
+    {
+      "title": "Sports Macros",
+      "link": "https://bonvee-nhl-macros.s3.us-west-1.amazonaws.com/dist/index.html",
+      "date": "Oct 2021 - Present",
+      "highlights": [
+        "Independently created a web application with TypeScript, tailored for media professionals",
+        "Facilitates effortless generation of macros, optimizing the media captioning workflow within Photo Mechanic",
+        "Utilized by professional NHL photographers during every Vancouver Canucks home game for the past 3 seasons"
+      ]
+    },
+    {
+      "title": "DLS",
+      "link": "https://github.com/bonvee-99/dls",
+      "date": "July 2023",
+      "highlights": [
+        "Co-created a Rust CLI designed for secure secret sharing among developers over Websockets",
+        "Implemented end-to-end encryption using RSA public/private key pairs to ensure the security of shared secrets",
+        "Introduced the ability for users to join rooms through dynamically generated URLs"
+      ]
+    }
+  ],
+  "education": {
+    "school": "University of British Columbia",
+    "location": "Vancouver, BC",
+    "degree": "Bachelor of Science, Computer Science",
+    "startDate": "Sept 2019",
+    "endDate": "April 2025",
+    "gpa": "3.9",
+    "coursework": [
+      "Intermediate Algorithm Design & Analysis",
+      "Computer Hardware & Operating Systems",
+      "Applied Machine Learning",
+      "Computer Networking",
+      "Parallel Computation",
+      "Distributed Systems",
+      "Computer Security"
+    ]
+  },
+  "skills": {
+    "languages": [
+      "Java",
+      "TypeScript",
+      "JavaScript",
+      "Go",
+      "Python",
+      "C",
+      "C++",
+      "Bash",
+      "SQL",
+      "NoSQL",
+      "GraphQL",
+      "HTML",
+      "CSS"
+    ],
+    "frameworks": [
+      "Node",
+      "Vue",
+      "React",
+      "JUnit",
+      "Mocha",
+      "Chai"
+    ],
+    "other": [
+      "Git",
+      "Docker",
+      "Kubernetes",
+      "GCP",
+      "AWS",
+      "Terraform",
+      "Jenkins",
+      "Datadog",
+      "Jira"
+    ]
+  }
+}
