b3d27cb07423c4305a9b1a91879c2465db419baf
Benjamin Vinnick <bvinnick@student.ubc.ca>
2025-05-03 12:18:32 -0700

    site map

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://benvinnick.com'
+  site: 'https://benvinnick.com',
+  integrations: [sitemap()]
 });
